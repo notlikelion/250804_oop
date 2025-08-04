519a03d01d340ab09d198c4e4b2188d3142f636d
notlikelion <notlikelion@gmail.com>
2025-08-04 17:16:56 +0900

    푸시

--- src/step4/GeminiVer3.java
+++ src/step4/GeminiVer3.java
@@ -17,8 +17,10 @@ public class GeminiVer3 {
 //        Chatbot chatbot = new RoleChatbot(apiKey);
 //        Chatbot chatbot = new RoleChatbot(apiKey, "100자 이내로, MBTI가 INTJ인 사람처럼 대답해줘.");
         Scanner sc = new Scanner(System.in);
-        System.out.print("당신과 상담할 사람을 서술해보세요 : ");
-        Chatbot chatbot = new RoleChatbot(apiKey, sc.nextLine() + "라고 설명된 사람이 대답하는 듯이 해줘.");
+//        System.out.print("당신과 상담할 사람을 서술해보세요 : ");
+//        Chatbot chatbot = new RoleChatbot(apiKey, sc.nextLine() + "라고 설명된 사람이 대답하는 듯이 해줘.");
+//        Chatbot chatbot = new EnglishChatbot(apiKey);
+        Chatbot chatbot = new EnglishChatbot(apiKey, "일본어");
         while (true) {
             System.out.print("질문을 입력하세요 : ");
             String question = sc.nextLine();
