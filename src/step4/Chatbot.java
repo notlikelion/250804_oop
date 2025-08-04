4456b169626a20ff9dadeda71bdc2300706f4f7b
notlikelion <notlikelion@gmail.com>
2025-08-04 16:36:49 +0900

    설계

--- src/step4/Chatbot.java
+++ src/step4/Chatbot.java
@@ -13,6 +13,24 @@ public abstract class Chatbot implements IGemini {
     public String chat(String message) throws Exception {
         // chat -> apiKey.
         System.out.println(apiKey);
+        // 1. 요청 받은 메시지를 다듬거나 추가. Role, 가이드를 추가
+        String templateMessage = handleMessage(message);
+        // 2. GEMINI 통신
+        String geminiResponse = callGemini(apiKey, templateMessage);
+        // 3. 응답 받는 텍스트를 다듬는.
+        String changedText = changeResult(geminiResponse);
         return "챗챗";
+    }
+
+    private String handleMessage(String message) {
+        return message;
+    }
+
+    private String callGemini(String apiKey, String text) {
+        return text;
+    }
+
+    private String changeResult(String result) {
+        return result;
     }
 }
